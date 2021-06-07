98b6540ad80c78ef9badbb11e6bde6fedb1698f7
Anthonyny019 <anthony.genesino@etec.sp.gov.br>
2021-06-07 05:25:35 +0000

    Hooks - Introducao (Aula 01)
    Signed-off-by: Anthonyny019 <anthony.genesino@etec.sp.gov.br>

---
+++ hooks/src/views/contents/NaoEncontrado.jsx
@@ -0,0 +1,10 @@
+import './NaoEncontrado.css'
+import React from "react"
+
+const NaoEncontrado = props => (
+    <div className="erro">
+        <h1><u>Página não encontrada.</u>😞 </h1>
+    </div>
+)
+
+export default NaoEncontrado
